bf1bbafa740b7a0f52c38384418ef064273d6011
Adi Chondro <gladichondro@gmail.com>
2026-06-28 23:45:23 +0700

    refactor: replace conditional DataState handling with fold

--- lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
@@ -1,25 +1,28 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:news_app_clean_architecture/core/resources/data_state.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/get_article.dart';
 import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
 import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
 
 class RemoteArticlesBloc extends Bloc<RemoteArticlesEvent, RemoteArticleState> {
   final GetArticleUseCase _getArticleUseCase;
-  RemoteArticlesBloc(this._getArticleUseCase) : super(const RemoteArticlesLoading()) {
+  RemoteArticlesBloc(this._getArticleUseCase)
+    : super(const RemoteArticlesLoading()) {
     on<GetArticles>(onGetArticles);
   }
 
-  Future<void> onGetArticles(GetArticles events, Emitter<RemoteArticleState> emit) async {
+  Future<void> onGetArticles(
+    GetArticles events,
+    Emitter<RemoteArticleState> emit,
+  ) async {
     final dataState = await _getArticleUseCase();
 
-    if(dataState is DataSuccess && dataState.data!.isNotEmpty){
-      emit(RemoteArticlesDone(dataState.data!));
-      
-    }
-    
-    if(dataState is DataFailed) {
-      emit(RemoteArticlesError(dataState.error!));
-    }
+    dataState.fold(
+      (error) {
+        emit(RemoteArticlesError(error));
+      },
+      (data) {
+        emit(RemoteArticlesDone(data));
+      },
+    );
   }
 }
