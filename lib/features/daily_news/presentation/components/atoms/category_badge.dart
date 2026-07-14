973be96ad35b033dce311817d3a500da37b20966
Adi Chondro <gladichondro@gmail.com>
2026-07-14 16:13:38 +0700

    refactor(design-system): replace hardcoded padding with AppSpacing tokens in chips

--- lib/features/daily_news/presentation/components/atoms/category_badge.dart
+++ lib/features/daily_news/presentation/components/atoms/category_badge.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_app_clean_architecture/core/presentation/atoms/app_colors.dart';
 import 'package:news_app_clean_architecture/core/presentation/atoms/app_radius.dart';
+import 'package:news_app_clean_architecture/core/presentation/atoms/app_spacing.dart';
 import 'package:news_app_clean_architecture/core/presentation/atoms/app_typography.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
@@ -12,7 +13,10 @@ class CategoryBadge extends StatelessWidget {
   Widget build(BuildContext context) {
     return Skeleton.replace(
       child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+        padding: const EdgeInsets.symmetric(
+          horizontal: AppSpacing.stackMd,
+          vertical: AppSpacing.stackSm,
+        ),
         decoration: ShapeDecoration(
           color: AppColors.secondaryContainer,
           shape: RoundedRectangleBorder(
